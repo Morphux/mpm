3cf129635e2bd62934cbbdc195b82254d4310606
Ne02ptzero <louis@ne02ptzero.me>
2017-08-12 01:15:31 +0200

    Fix(Tests): Now removing all possibles configuration from the system

--- tests/config/config_no_config.cmd
+++ tests/config/config_no_config.cmd
@@ -1,1 +1,1 @@
-rm -f /home/chrx/.config/mpm/mpm.conf && ./mpm config -l ; cp ./lib/libmpm/tests/conf/base.conf /home/chrx/.config/mpm/mpm.conf
+rm -f /home/chrx/.config/mpm/mpm.conf  && rm -f ~/.mpm/mpm.conf && rm -rf /etc/mpm.conf && ./mpm config -l ; cp ./lib/libmpm/tests/conf/base.conf /home/chrx/.config/mpm/mpm.conf
